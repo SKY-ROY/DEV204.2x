11eb3f0c7873c556c53d21fa7b5a251e5ecec6cb
SKY <akash.rm.roy@gmail.com>
2020-03-16 22:32:13 +0530

    Create Student.cs

---
+++ Mod2_SelfAssesmentLab/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_SelfAssesmentLab
+{
+    class Student : Person
+    {
+        private int studentID;
+        public int StudentID
+        {
+            get { return studentID; }
+            set { studentID = value; }
+        }
+        public Student(string name, int ID, int yy, int mm, int dd) : base(name, yy, mm, dd)
+        {
+            this.StudentID = ID;
+        }
+        public void TakeTest()
+        {
+            Console.WriteLine("Test taken.");
+        }
+        public override void GetStatus()
+        {
+            Console.WriteLine("[Student] Name: " + this.Name + " DOB: " + this.DOB.ToString() + " Age: " + this.Age + " ID: " + this.StudentID);
+        }
+    }
+}
