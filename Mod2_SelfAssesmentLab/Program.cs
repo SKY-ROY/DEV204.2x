1dab286420afecfd47bb9dad22ba38ebd440ee34
SKY <akash.rm.roy@gmail.com>
2020-03-16 22:32:18 +0530

    Create Program.cs

---
+++ Mod2_SelfAssesmentLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_SelfAssesmentLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher t1 = new Teacher("Prof. Seri", 16022020, 1988, 02, 02);
+            Student s1 = new Student("Sky", 01121999, 1998, 06, 05);
+            
+            t1.GetStatus();
+            s1.GetStatus();
+        }
+    }
+}
