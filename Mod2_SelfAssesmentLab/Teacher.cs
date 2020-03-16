51fcc782714db73857af15ed7764a258dcb49e6e
SKY <akash.rm.roy@gmail.com>
2020-03-16 22:32:08 +0530

    Create Teacher.cs

---
+++ Mod2_SelfAssesmentLab/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_SelfAssesmentLab
+{
+    class Teacher : Person
+    {
+        private int teacherID;
+        public int TeacherID
+        {
+            get { return teacherID; }
+            set { teacherID = value; }
+        }
+        public Teacher(string name, int ID, int yy, int mm, int dd) : base(name, yy, mm, dd)
+        {
+            this.TeacherID = ID;
+        }
+        public void GradeTest()
+        {
+            Console.WriteLine("Test graded.");
+        }
+        public override void GetStatus()
+        {
+            Console.WriteLine("[Teacher] Name: " + this.Name + " DOB: " + this.DOB.ToString() + " Age: " + this.Age + " ID: " + this.TeacherID);
+        }
+    }
+}
